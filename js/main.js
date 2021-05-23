becaa41df2af4210d406917376e5244bf989b585
Slava <vierosprojumpercs@mail.ru>
2021-05-23 22:14:49 +0500

    second commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const products = [
     { id: 3, title: 'Keyboard', price: 250 },
     { id: 4, title: 'Gamepad', price: 150 },
 ];
-
 const renderProduct = ({ title, price }) => {
     return `<div class="product-item">
                 <h3>${title}</h3>
@@ -13,7 +12,6 @@ const renderProduct = ({ title, price }) => {
                 <button class="by-btn">Добавить</button>
               </div>`;
 }
-
 const renderProducts = (arr) => {
     let productList = ''
     arr.forEach(item => {
